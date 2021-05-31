c8b9cdf742a0c9cbb8f20e5ce777d7bd868d79e5
theincognito-inc <imags.bondoo7@gmail.com>
2021-05-31 16:17:33 +0530

    Changes for kebab

---
+++ README.md
@@ -0,0 +1,65 @@
+# Device Tree for 8T (kebab) for TWRP
+## Disclaimer - Unofficial TWRP!
+These are personal test builds of mine. In no way do I hold responsibility if it/you messes up your device.
+Proceed at your own risk.
+
+## Setup repo tool
+Setup repo tool from here https://source.android.com/setup/develop#installing-repo
+
+## Compile
+
+Sync aosp_r29 manifest:
+
+```
+repo init --depth=1 --partial-clone --clone-filter=blob:limit=10M -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r29
+```
+
+Make a directory named local_manifest under .repo, and create a new manifest file, for example instantnoodle.xml
+and then paste the following
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+<remote name="github"
+	fetch="https://github.com/" />
+
+<project path="device/oneplus/instantnoodle"
+	name="theincognito/android_device_oneplus_kebab"
+	remote="github"
+	revision="android-11" />
+</manifest>
+```
+Use https://del.dog/a11-twrp-extras-updated.txt is same directory as well. You might need to pick few patches from gerrit.twrp.me to get some stuff working.
+
+Sync the sources with
+
+```
+repo sync -j$(nproc --all)
+```
+
+To build, execute these commands in order
+
+```
+. build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL=C
+lunch aosp_kebab-eng
+make -j$(nproc --all) adbd recoveryimage
+```
+
+To test it:
+
+```
+# To temporarily boot it
+fastboot boot out/target/product/kebab/recovery.img 
+
+# Since 8 / Pro has a seperate recovery partition, you can flash the recovery with
+fastboot flash recovery recovery.img
+```
+
+##### Credits
+- bigbiff for decryption
+- Systemad for original tree
+- CaptainThrowback for original tree
+- mauronofrio for original tree
+- TWRP team
---
+++ device.mk
@@ -0,0 +1,74 @@
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+LOCAL_PATH := device/oneplus/kebab
+
+# define hardware platform
+PRODUCT_PLATFORM := kona
+
+# A/B support
+AB_OTA_UPDATER := true
+
+# A/B updater updatable partitions list. Keep in sync with the partition list
+# with "_a" and "_b" variants in the device. Note that the vendor can add more
+# more partitions to this list for the bootloader and radio.
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+		system_ext \
+    vendor \
+    vbmeta \
+		dtbo
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# tell update_engine to not change dynamic partition table during updates
+# needed since our qti_dynamic_partitions does not include
+# vendor and odm and we also dont want to AB update them
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd \
+    resetprop
+
+# qcom decryption
+PRODUCT_PACKAGES_ENG += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# tzdata
+PRODUCT_PACKAGES_ENG += \
+    tzdata_twrp
+
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
