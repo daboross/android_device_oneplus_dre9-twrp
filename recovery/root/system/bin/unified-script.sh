2c9ec7a74df423b52166b956f732960a0f421d95
theincognito-inc <imags.bondoo7@gmail.com>
2021-06-18 04:27:20 +0530

    Added more props to reset

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -25,26 +25,41 @@ load_op8()
 
 load_op8t()
 {
+    resetprop "ro.build.product" "OnePlus8T"
+    resetprop "ro.display.series" "OnePlus 8T"
+    resetprop "ro.product.device" "OnePlus8T"
     resetprop "ro.product.model" "OnePlus 8T"
     resetprop "ro.product.name" "OnePlus8T"
-    resetprop "ro.build.product" "OnePlus8T"
-    resetprop "ro.product.device" "OnePlus8T"
+    resetprop "ro.product.odm.device" "OnePlus8T"
+    resetprop "ro.product.odm.model" "OnePlus 8T"
+    resetprop "ro.product.odm.name" "OnePlus8T"
+    resetprop "ro.product.product.device" "OnePlus8T"
+    resetprop "ro.product.product.model" "OnePlus 8T"
+    resetprop "ro.product.product.name" "OnePlus8T"
+    resetprop "ro.product.system.device" "OnePlus8T"
+    resetprop "ro.product.system.model" "OnePlus 8T"
+    resetprop "ro.product.system.name" "OnePlus8T"
+    resetprop "ro.product.system_ext.device" "OnePlus8T"
+    resetprop "ro.product.system_ext.model" "OnePlus 8T"
+    resetprop "ro.product.system_ext.name" "OnePlus8T"
+    resetprop "ro.product.vendor.device" "OnePlus8T"
+    resetprop "ro.product.vendor.model" "OnePlus 8T"
+    resetprop "ro.product.vendor.name" "OnePlus8T"
     resetprop "ro.vendor.product.device" "OnePlus8T"
-    resetprop "ro.display.series" "OnePlus 8T"
 }
 
-project=$(getprop ro.boot.project_name)
-echo $project
+project=$(getprop ro.boot.project_codename)
+echo "Running unified/variant script with $project..." >> /tmp/recovery.log
 
 case $project in
-    "19821")
-        load_op8
+    instantnoodlep*)
+        load_op8pro
         ;;
-    "19805")
+    kebab*)
         load_op8t
         ;;
     *)
-        load_op8pro
+        load_op8
         ;;
 esac
 
